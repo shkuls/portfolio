e30f70264ef21f5456a804ab815ff589567dba4b
shkuls <diy2903@gmail.com>
2024-01-12 03:47:55 +0530

    added typewriter

--- frontend/src/pages/Intro.tsx
+++ frontend/src/pages/Intro.tsx
@@ -1,33 +1,50 @@
 import { useEffect } from "react";
 import Topbar from "../components/Topbar"
 import Spline from '@splinetool/react-spline';
+import { TypeAnimation } from 'react-type-animation';
+
 export default function Intro() {
     useEffect(()=>{
         window.scrollTo(0, 0);
     } , [])
-  return (<>
+
+  return (
+  <>
 
     <Topbar/>
-    <div className="bg-[#FF5760] flex flex-col sm:flex-row" id="home">
-        <div className="m-auto min-w-80 w-[70vw] flex-col ml-10">
-            <p>
+    <div className="bg-[#FF5760] flex flex-col sm:flex-row pl-10" id="home">
+        <div className="m-auto min-w-80 w-[100vw] flex-col ml-10 font-poppins tracking-wider">
+            <p className="font-semibold text-2xl mb-">
                 Hi there!
             </p>
-            <p className="text-5xl font-bold">
+            <TypeAnimation
+                sequence={[
+                "I'm Shlok",
+                1000,
+                "I'm a Developer",
+                1000
+                
+                ]}
+                speed={50}
+                repeat={Infinity}
+                style={{ fontSize: '50px' , fontWeight : '900' , fontFamily : 'Poppins'}}
+            />
+            
+            
+            {/* <p className="text-8xl font-bold tracking-widest" id="target">
                 I'm Shlok.
-            </p>
-            <p className="text-2xl">
+            </p> */}
+            <p className="text-3xl w-5/6">
                 I make applications for the web.
             </p>
 
 
         </div>
-        <Spline scene="https://prod.spline.design/DCNLvIodaiM-1hq9/scene.splinecode" />
+        
+        <Spline className="" scene="https://prod.spline.design/DCNLvIodaiM-1hq9/scene.splinecode" />
 
 
     </div>
-
-  
-  </>
+</>
   )
 }
--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -5,7 +5,7 @@ import { ProjectArray } from "../assets/Projects/ProjectArray.js"
 export default function Projects() {
   return (
         <div className='bg-[#4A28FF]' id="projects">
-            <h1 className="mx-auto w-80 font-bold text-5xl text-center tracking-widest transition-all ">PROJECTS</h1>
+            <h1 className="mx-auto w-80 font-bold text-5xl text-center tracking-widest transition-all  hover:text-neutral-50">PROJECTS</h1>
             <div className="flex flex-row flex-wrap justify-evenly">
             {
               ProjectArray.map((project , index)=>
